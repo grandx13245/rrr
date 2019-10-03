e249710e2262d69f3a952eb67fc53eafa043054e
LN <LN@gmail.com>
2019-10-03 15:06:25 +0800

    UDP client網路程式

---
+++ src/LN/knu/edu/tw/Main.java
@@ -0,0 +1,18 @@
+package LN.knu.edu.tw;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) {
+        String HOST = "tw.yahoo.com";
+        String IP = "124.108.103.103";
+
+        try {
+            InetAddress addr = InetAddress.getByName(HOST);
+            String ip = addr.getHostAddress();
+            System.out.println("ip: " + ip);
+        }catch (UnknownHostException e){
+        }
+    }
+}
